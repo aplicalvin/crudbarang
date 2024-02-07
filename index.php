4004259ab96e0f11745bf845b5098f29f69f69e2
aplicalvin <thecalvins.sims@gmail.com>
2024-02-07 14:41:05 +0700

    Update V.2.4 - Perbaikan indentasi

--- index.php
+++ index.php
@@ -32,9 +32,11 @@ $sql = "SELECT * FROM $table";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
+    $counter = 1; // Inisialisasi counter
     $table_content .= "<table class='table'><thead><tr><th scope='col'>No</th><th scope='col'>Kode</th><th scope='col'>Nama Barang</th><th scope='col'>Satuan</th><th scope='col'>Jumlah</th><th scope='col'>Tanggal Beli</th><th scope='col'>Harga</th><th scope='col'>Action</th></tr></thead><tbody>";
     while ($row = $result->fetch_assoc()) {
-        $table_content .= "<tr><th scope='row'>" . $row['id'] . "</th><td>" . $row['kode'] . "</td><td>" . $row['nama_brg'] . "</td><td>" . $row['satuan'] . "</td><td>" . $row['jumlah'] . "</td><td>" . $row['tgl_beli'] . "</td><td>" . $row['harga'] . "</td><td><button type='button' class='btn btn-primary' onclick='openEditModal(" . $row['id'] . ")'>Edit</button> <button type='button' class='btn btn-danger' onclick='openRemoveModal(" . $row['id'] . ")'>Remove</button></td></tr>";
+        $table_content .= "<tr><th scope='row'>" . $counter . "</th><td>" . $row['kode'] . "</td><td>" . $row['nama_brg'] . "</td><td>" . $row['satuan'] . "</td><td>" . $row['jumlah'] . "</td><td>" . $row['tgl_beli'] . "</td><td>" . $row['harga'] . "</td><td><button type='button' class='btn btn-primary' onclick='openEditModal(" . $row['id'] . ")'>Edit</button> <button type='button' class='btn btn-danger' onclick='openRemoveModal(" . $row['id'] . ")'>Remove</button></td></tr>";
+        $counter++; // Increment counter
     }
     $table_content .= "</tbody></table>";
 } else {
